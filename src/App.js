410c9f6ebaf91579384d2a152692ba79095b2318
madalyn <madalynelaine@gmail.com>
2022-06-06 11:27:35 -0400

    content, add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>photo</div>
+        <div>
+          <h1 className="title">Madalyn Coryea</h1>
+        </div>
+        <div>
+          <p>
+            I am a Software Engineer with over 8 years experience.
+            Professionally I've done React, C#/.NET, Android, and iOS (but back
+            in the Objective-C days). I'm interested in UX/UI design and
+            front-end work.
+          </p>
+          <p>
+            I am a Worcester Polytechnic Institute (WPI) Alum. I was the
+            President of Women in Computer Science for two years, and our
+            membership quadrupled under my tenure.
+          </p>
+          <p>
+            I also enjoy writing, playing piano and ukelele, gardening, mushroom
+            foraging and cooking.
+          </p>
+        </div>
       </header>
+      <div>
+        <h1>Projects</h1>
+        <div>
+          <h2>Peachplex</h2>
+          <h2>Bonko</h2>
+          <h2>Github</h2>
+        </div>
+        <p>photo strip at bottom of page.</p>
+      </div>
     </div>
   );
 }
