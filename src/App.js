a3ed0ac720471abec632b536cc189ca3f7de5e91
madalyn <madalynelaine@gmail.com>
2022-06-06 12:15:51 -0400

    css

--- src/App.js
+++ src/App.js
@@ -5,26 +5,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>photo</div>
-        <div>
-          <h1 className="title">Madalyn Coryea</h1>
-        </div>
-        <div>
-          <p>
-            I am a Software Engineer with over 8 years experience.
-            Professionally I've done React, C#/.NET, Android, and iOS (but back
-            in the Objective-C days). I'm interested in UX/UI design and
-            front-end work.
-          </p>
-          <p>
-            I am a Worcester Polytechnic Institute (WPI) Alum. I was the
-            President of Women in Computer Science for two years, and our
-            membership quadrupled under my tenure.
-          </p>
-          <p>
-            I also enjoy writing, playing piano and ukelele, gardening, mushroom
-            foraging and cooking.
-          </p>
+        <div className="d-flex flex-fill justify-content-between">
+          <div className="d-flex w-100 justify-content-center align-items-center">
+            <div className="header-photo">photo</div>
+          </div>
+          <div>
+            <h1 className="title mb-4">Madalyn Coryea</h1>
+            <div className="mt-4 header-width">
+              <p>
+                I am a Software Engineer with over 8 years experience.
+                Professionally I've done React, C#/.NET, Android, and iOS (but
+                back in the Objective-C days). I'm interested in UX/UI design
+                and front-end work.
+              </p>
+              <p>
+                I am a Worcester Polytechnic Institute (WPI) Alum. I was the
+                President of Women in Computer Science for two years, and our
+                membership quadrupled under my tenure.
+              </p>
+              <p>
+                I also enjoy writing, playing piano and ukelele, gardening,
+                mushroom foraging and cooking.
+              </p>
+            </div>
+          </div>
         </div>
       </header>
       <div>
